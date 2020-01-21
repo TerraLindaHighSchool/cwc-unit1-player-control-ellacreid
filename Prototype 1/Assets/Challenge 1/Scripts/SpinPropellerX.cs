bfedad86fbabaeaa0628a8e62c2b282f84618d9e
ellacreid <59098975+ellacreid@users.noreply.github.com>
2020-01-21 00:08:10 -0800

    Tutorial challenge 1.8 in progress

---
+++ Prototype 1/Assets/Challenge 1/Scripts/SpinPropellerX.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinPropellerX : MonoBehaviour
+{
+    public float verticalInput;
+    public float turnSpeed = 10.0f;
+    public float target = player.transform.position;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        verticalInput = Input.GetAxis("Vertical");
+        transform.RotateAround(target, Vector3.up, verticalInput * turnSpeed * Time.deltaTime);
+    }
+}
